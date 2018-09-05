01cca55ac4797c3209ecfb7d957354583aee4e39
ion utale <ion.utale@icloud.com>
2018-09-05 21:50:24 +0200

    fixing naming issue

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -61,7 +61,7 @@ process.on('SIGTERM', function () {
 
 // BRING IN YOUR SCHEMAS & MODELS
 require('./models/user');
-require('./models/Vehicle2');
-require('./models/Product2');
+require('./models/vehicle');
+require('./models/product');
 require('./models/maintenance');
 
