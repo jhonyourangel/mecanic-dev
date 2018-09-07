fa366c1d4d75172a68fae90f22e922a728b30eb0
ion utale <ion.utale@icloud.com>
2018-09-07 05:46:38 +0200

    remove console.log

--- app_api/database/queries/vehicle.js
+++ app_api/database/queries/vehicle.js
@@ -3,9 +3,7 @@ const Vehicle = mongoose.model('vehicle');
 const errorHandler = require('../db-error-handler');
 
 module.exports.createVehicle = (req, res, next) => {
-    // console.log({...req.body})
-    let newVehicle = new Vehicle({ ...req.body
-    })
+    let newVehicle = new Vehicle({ ...req.body })
     newVehicle.save()
         .then(saveRes => res.status(200).json(saveRes))
         .catch(err => errorHandler(err, req, res, next))
