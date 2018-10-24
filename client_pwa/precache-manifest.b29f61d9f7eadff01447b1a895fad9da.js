19fe54aacece3f0597fcf1dcac6bbe63021cceb1
ion utale <ion.utale@icloud.com>
2018-10-24 19:37:43 +0200

    new front end

---
+++ client_pwa/precache-manifest.b29f61d9f7eadff01447b1a895fad9da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03657f373e48fc2bc020",
+    "url": "/static/js/main.03657f37.chunk.js"
+  },
+  {
+    "revision": "dec6f11fe755ae9649c7",
+    "url": "/static/js/1.dec6f11f.chunk.js"
+  },
+  {
+    "revision": "03657f373e48fc2bc020",
+    "url": "/static/css/main.1e0ea271.chunk.css"
+  },
+  {
+    "revision": "566c2240efbe88bf543e095add74d668",
+    "url": "/index.html"
+  }
+];
