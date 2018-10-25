026f4ccbb6a17166040c6c30752699a10d5f1223
ion utale <ion.utale@icloud.com>
2018-10-25 14:51:34 +0200

    new front end

---
+++ client_pwa/precache-manifest.82816f6ec271f22491723a918da23b72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee577af711b72721b3f2",
+    "url": "/static/js/main.ee577af7.chunk.js"
+  },
+  {
+    "revision": "4b2f0daf302a9306c200",
+    "url": "/static/js/1.4b2f0daf.chunk.js"
+  },
+  {
+    "revision": "ee577af711b72721b3f2",
+    "url": "/static/css/main.8f7b0f11.chunk.css"
+  },
+  {
+    "revision": "9451f5022b5aaadefd749d63dba0c9ba",
+    "url": "/index.html"
+  }
+];
--- client_pwa/service-worker.js
+++ client_pwa/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48f62d6fca74a05f6d82af47f4d68aba.js"
+  "/precache-manifest.82816f6ec271f22491723a918da23b72.js"
 );
 
 workbox.clientsClaim();
